ab2a5212d55337c8d3c3478b1ca0bd40627eed52
GermanVan <supergera95@gmail.com>
2024-11-11 14:52:00 -0800

    Initial commit

---
+++ swiftui!/swiftui_App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_App.swift
+//  swiftui!
+//
+//  Created by Герман Ван on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui!Tests/swiftui_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_Tests.swift
+//  swiftui!Tests
+//
+//  Created by Герман Ван on 11.11.2024.
+//
+
+import Testing
+@testable import swiftui_
+
+struct swiftui_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

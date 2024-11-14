ca8d3818c3e6a3a2f2a98a4910bad9cf4eab36a5
GermanVan <supergera95@gmail.com>
2024-11-14 08:31:04 -0800

    Delete Balls.playground directory

--- Balls.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-import PlaygroundSupport
-import UIKit
-
-let sizeOfArea = CGSize(width: 400, height: 400)
-
-var area = SquareArea(size: sizeOfArea, color: UIColor.gray)
-area.setBalls(withColors: [UIColor.red, UIColor.orange, UIColor.green, UIColor.blue], andRadius: 30)
-PlaygroundPage.current.liveView = area
-
